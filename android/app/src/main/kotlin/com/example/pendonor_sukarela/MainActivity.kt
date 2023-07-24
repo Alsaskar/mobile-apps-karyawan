1b63e4e67ceca2bbf01fc1e4c72d0d21e7a474a5
kendy29 <kendyman2908@gmail.com>
2023-07-24 14:57:04 +0800

    update

--- android/app/src/main/kotlin/com/example/pendonor_sukarela/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pendonor_sukarela
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/services/constanst.dart
+++
@@ -1,4 +0,0 @@
-class ApiConstants {
-  static String baseUrl = 'http://192.168.140.42:8080/api/v1';
-  static String baseUrlStatic = 'http://192.168.140.42:8080';
-}
